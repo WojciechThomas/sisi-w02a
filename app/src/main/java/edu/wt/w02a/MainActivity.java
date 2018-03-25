7cd8e1137b08060495e3758199a50bf76e3f5ade
Wojtek <wojciech.thomas@pwr.edu.pl>
2018-03-25 11:31:17 +0200

    ActionBar -> AppCompat

--- app/src/main/java/edu/wt/w02a/MainActivity.java
+++ app/src/main/java/edu/wt/w02a/MainActivity.java
@@ -1,8 +1,8 @@
 package edu.wt.w02a;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     public static final String MY_SETTINGS = "MySettings";
 
 
